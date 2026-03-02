61cddc927053a606ebc161191991bdbce7e4ec59
rahat <rakibulalamrahat24@gmail.com>
2026-03-02 06:14:04 +0600

    fix:removed rnr screen + added better primary colors

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,28 @@
 import { Tabs } from "expo-router";
 import { AlarmClock } from "@/components/Icons";
 import { useColorScheme } from "@/lib/useColorScheme";
+import { PenBox } from "lucide-react-native";
 
-export const unstable_settings = {
-  initialRouteName: "index",
-};
 
 export default function TabLayout() {
   const { colors } = useColorScheme();
   return (
-    <Tabs>
+    <Tabs screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor: colors.primary,
+    }}>
       <Tabs.Screen
         name="index"
         options={{
           title: "",
           tabBarIcon: () => <AlarmClock size={24} color={colors.primary} />,
+        }}
+      />
+      <Tabs.Screen
+        name="test"
+        options={{
+          title: "",
+          tabBarIcon: () => <PenBox size={20} />,
         }}
       />
     </Tabs>
--- app/(tabs)/test.tsx
+++ app/(tabs)/test.tsx
@@ -1,15 +1,15 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Button } from "@/components/ui/button";
 import { Text } from "@/components/common/Text";
 import { View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
+import { Badge } from "@/components/ui/badge";
 
 type Props = {};
 export default function Alarms({ }: Props) {
   return (
-    <SafeAreaView className="flex-1 bg-background" >
-      <Text className="text-foreground"  >Alarms Component</Text>
+    <View className="flex-1 bg-background" >
+      <Text className="text-foreground"  >pen Component</Text>
       <ThemeToggle />
-    </SafeAreaView>
+    </View>
   );
 }
